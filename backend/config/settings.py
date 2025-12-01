11a1241d2c47bcd479510b7978ccc4461b6f6d25
Ved Thakar <ved06.thakar@gmail.com>
2025-11-30 19:11:33 -0500

    fix: fetch to django server

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -141,6 +141,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+
+    # keep these if you still use 5173 sometimes
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
